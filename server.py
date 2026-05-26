526f09aacfacc0571c4a7311d8c1e858425e5290
Rohit Gite <rohitgite1419@gmail.com>
2026-05-26 15:24:17 +0530

    fixed stateless flag

--- server.py
+++ server.py
@@ -14,8 +14,8 @@ configure_logging()
 logger = logging.getLogger("calendar-mcp-server")
 
 backend = HeaderCredentialBackend()
-mcp = FastMCP("MewCP Google Calendar MCP Server",
-    # stateless_http=True,
+mcp = FastMCP(
+    "MewCP Google Calendar MCP Server",
     middleware=[CredentialMiddleware(backend, "oauth")],
 )
 register_tools(mcp)
